c1859c037695d0882fd5b767cbbb156c24b90e84
Ethan_1000 <snowlaxc@gmail.com>
2023-12-22 21:13:03 +0900

    [level 2] Title: 올바른 괄호, Time: 7.53 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.py
@@ -0,0 +1,16 @@
+def solution(s):
+    left = 0
+    right = 0
+    for i in s:
+        if i == "(":
+            left += 1
+        else:
+            right += 1
+            
+        if left < right:
+            return False
+        
+    if left != right:
+        return False
+    else:
+        return True
