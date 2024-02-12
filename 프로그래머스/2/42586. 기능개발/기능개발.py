36e4a8479df5cf041f5f1bd1a52e648884198463
Ethan_1000 <snowlaxc@gmail.com>
2024-02-12 19:23:38 +0900

    [level 2] Title: 기능개발, Time: 0.03 ms, Memory: 10.1 MB -BaekjoonHub

---
+++ 프로그래머스/2/42586. 기능개발/기능개발.py
@@ -0,0 +1,28 @@
+from collections import deque, Counter
+
+def solution(progresses, speeds):
+    progresses = deque(progresses)
+    speeds = deque(speeds)
+    day = 0
+    now_prg = 0
+    now_spd = 0
+    answer = []
+
+    for _ in range(len(progresses)):
+        now_prg = progresses.popleft()
+        now_spd = speeds.popleft()
+        now_prg += now_spd * day
+        
+        while True:
+            if now_prg < 100:
+                day += 1
+                now_prg += now_spd
+            else:
+                answer.append(day)
+                break
+                
+    answer = sorted(Counter(answer).items(), key = lambda x:x[0], reverse = False)
+    
+    answer = list(i[1] for i in answer)
+        
+    return answer
