105a1bc2a45931df19312f33c75d229dfd45ba24
Ethan_1000 <snowlaxc@gmail.com>
2024-02-13 16:30:50 +0900

    [level 3] Title: 베스트앨범, Time: 19.46 ms, Memory: 57.4 MB -BaekjoonHub

--- 프로그래머스/3/42579. 베스트앨범/베스트앨범.py
+++ 프로그래머스/3/42579. 베스트앨범/베스트앨범.py
@@ -1,36 +1,13 @@
+import numpy as np
 import pandas as pd
-
-def solution(genres, plays):
-    df = pd.DataFrame({'genres': genres, 'plays': plays})
-    # print(df)
-    
-    total = df.groupby(['genres'], as_index = False).sum()
-    total.columns = ['genres', 'total']
-    # print(total)
-    
-    df = pd.merge(df, total, how = 'left')
-    # print(df)
-    
-    df = df.sort_values(['total', 'plays'], ascending = False)
-    df = df.reset_index()
-    # print(df)
-    
-    answer = []
-    now_genres = str('')
-    before_genres = str('')
-    count = 0
-    
-    for i in range(len(df)):
-        now_genres = df['genres'][i]
-        count += 1
-        
-        if before_genres == now_genres:
-            if count < 3:
-                answer.append(int(df['index'][i]))
-        else:
-            answer.append(int(df['index'][i]))
-            before_genres = now_genres
-            count = 1
-                
+def solution (genres, plays):
+    musics = pd. DataFrame ()
+    musics ['id'] = range (len (plays) )
+    musics ['plays'] = plays
+    musics ['genres'] = genres
+    musics ['genres_plays'] = musics.groupby ('genres').transform(np.sum)['plays']
+    musics['genres_head_id'] = musics.groupby ('genres'). transform(np.min) ['id']
+    musics ['order'] = musics['genres_plays']*1000000 - musics['genres_head_id' ]*1000 + musics['plays']
+    answer = musics.sort_values('order', ascending=False).groupby('genres').head(2)['id']
+    answer = list (answer)
     return answer
-        
