1ba3c965a72e0891a57e5bbfb4cf802dd3c590c3
Ethan_1000 <snowlaxc@gmail.com>
2024-02-27 16:06:37 +0900

    [level 2] Title: 피보나치 수, Time: 509.56 ms, Memory: 439 MB -BaekjoonHub

--- 프로그래머스/2/12945. 피보나치 수/피보나치 수.py
+++ 프로그래머스/2/12945. 피보나치 수/피보나치 수.py
@@ -1,7 +1,18 @@
 def solution(n):
-    fibo_list = [0, 1]
+    dp = [0] * (n + 1)
+    dp[1] = 1
     
-    for _ in range(n - 1):
-        fibo_list.append(fibo_list[-1] + fibo_list[-2])
+    for i in range(2, n + 1):
+        dp[i] = dp[i-1] + dp[i-2]
         
-    return fibo_list[n] % 1234567
+    # print(dp)
+    
+    return dp[-1] % 1234567
+
+# def solution(n):
+#     fibo_list = [0, 1]
+    
+#     for _ in range(n - 1):
+#         fibo_list.append(fibo_list[-1] + fibo_list[-2])
+        
+#     return fibo_list[n] % 1234567
