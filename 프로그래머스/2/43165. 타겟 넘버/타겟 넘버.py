f60bce6d19dddc67200277067c35543441b51b2e
Ethan_1000 <snowlaxc@gmail.com>
2024-03-21 18:01:57 +0900

    [level 2] Title: 타겟 넘버, Time: 3.18 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.py
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.py
@@ -1,15 +1,16 @@
 def solution(numbers, target):
-    global answer
     answer = 0
+    len_numbers = len(numbers)
     
-    DFS(numbers, target, 0, 0)
+    def DFS(time, result):
+        if time != len_numbers:
+            DFS(time + 1, result + numbers[time])
+            DFS(time + 1, result - numbers[time])
+        else:
+            if result == target:
+                nonlocal answer
+                answer += 1
+        
+    DFS(0, 0)
+    
     return answer
-
-def DFS(numbers, target, time, result):
-    if (len(numbers) == time):
-        if (target == result):
-            global answer
-            answer += 1
-    else:
-        DFS(numbers, target, time + 1, result + numbers[time])
-        DFS(numbers, target, time + 1, result - numbers[time])
