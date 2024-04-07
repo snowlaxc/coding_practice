6a6d295023a9e156cfcdd0defecddd0916efcd91
Ethan_1000 <snowlaxc@gmail.com>
2024-04-07 22:58:08 +0900

    [level 2] Title: H-Index, Time: 0.01 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/2/42747. H－Index/H－Index.py
@@ -0,0 +1,10 @@
+from collections import Counter
+
+def solution(citations):
+    citations.sort(reverse = True)
+    
+    for i in range(len(citations)):
+        if (citations[i] < i+1):
+            return i
+    
+    return len(citations)
