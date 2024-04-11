b329987efdad0668a40b78ca6b1b98f2446b41d4
Ethan_1000 <snowlaxc@gmail.com>
2024-04-11 19:33:46 +0900

    [level 1] Title: 실패율, Time: 0.03 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/1/42889. 실패율/실패율.py
+++ 프로그래머스/1/42889. 실패율/실패율.py
@@ -1,15 +1,28 @@
-def solution(N, stages):
-    answer = []
-    
-    ratio = {}
-    allPlayer = len(stages) 
-    
-    for i in range(1,N+1):
-        if allPlayer == 0: 
-            ratio[i] = 0
-        else:
-            ratio[i] = stages.count(i)/allPlayer
-            allPlayer -= stages.count(i) 
-    answer = sorted(ratio, key=lambda x : ratio[x],reverse=True)
+from collections import Counter
 
-    return answer
+def solution(N, stages):
+    all_users = len(stages)
+    stages_temp = dict(Counter(stages))
+    fail_user = {}
+    for i in range(1, N+2):
+        fail_user[i] = stages_temp.get(i, 0)
+    # print(fail_user)
+    
+    challenge_user = {}
+    for stage, failed in fail_user.items():
+        challenge_user[stage] = all_users
+        all_users -= failed
+    # print(challenge_user)
+    
+    fail_perc = {}
+    for i in range(1, N+1):
+        if challenge_user[i] == 0:
+            fail_perc[i] = 0
+        else:
+            fail_perc[i] = fail_user[i]/challenge_user[i]
+    # print(fail_perc)
+    
+    fail_perc = sorted(fail_perc.items(), key = lambda x:x[1], reverse = True)
+    # print(fail_perc)
+    
+    return [i[0] for i in fail_perc]
