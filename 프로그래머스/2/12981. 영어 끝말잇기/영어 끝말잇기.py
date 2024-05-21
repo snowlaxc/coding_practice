364468c65321cb1873764fa973b223464305dc4a
Ethan_1000 <snowlaxc@gmail.com>
2024-05-21 16:26:44 +0900

    [level 2] Title: 영어 끝말잇기, Time: 0.16 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/2/12981. 영어 끝말잇기/영어 끝말잇기.py
+++ 프로그래머스/2/12981. 영어 끝말잇기/영어 끝말잇기.py
@@ -1,31 +1,30 @@
+from collections import deque
+
 def solution(n, words):
-    time = 0    # 라운드 카운팅
-    before = words[0][0]    # 전 단어의 끝단어 세팅 (초기값은 첫단어 첫글자)
-    done = []    # 사용 단어 리스트
+    words = deque(words)
+    already = []
     
-    error = 0    # 틀렸는지 확인
-    result = [0,0]    # result 값 생성
+    now = words.popleft()
+    already.append(now)
     
-    for _ in range(len(words)):    # 총 라운드 세팅
+    end_alp = now[-1]
+    rounds = 1
+    time = 1
+    
+    while words:
         time += 1
-        i = words.pop(0)
-        
-        if before == i[0]:    # 끝말잇기가 정상 진행될 경우
-            before = i[-1]    # 다음 라운드를 위해 끝단어 세팅
-        else:
-            error = 1
-            break
-        
-        if i not in done:    # 이미 한 단어도 아닐 경우
-            done.append(i)
-        else:
-            error = 1
-            break
+        now = words.popleft()
+        if time > n:
+            time = 1
+            rounds += 1
+        if end_alp == now[0] and now not in already:
+            already.append(now)
+            end_alp = now[-1]
             
-    if error == 1:    # 탈락할 경우
-        result = [int(time % n), int((time + (n - 1)) // n)]    # result 값 세팅
+        else:
+            return time, rounds
+    
+    return [0, 0]
+    
+    
         
-        if result[0] == 0:
-            result[0] = n
-            
-    return result
