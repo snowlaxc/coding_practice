c38dbc1814098ac7a8211fef18d91052d1e74151
Ethan_1000 <snowlaxc@gmail.com>
2024-05-29 23:25:36 +0900

    [level 3] Title: 가장 먼 노드, Time: 25.22 ms, Memory: 20.3 MB -BaekjoonHub

--- 프로그래머스/3/49189. 가장 먼 노드/가장 먼 노드.py
+++ 프로그래머스/3/49189. 가장 먼 노드/가장 먼 노드.py
@@ -1,23 +1,30 @@
 from collections import deque
 
-def solution(n, edge):
-    answer = 0
-    graph = [[] for _ in range(n + 1)]
-    visited = [0] * (n + 1)
-    for a, b in edge:
+def solution(n, vertex):
+    
+    visited = [False] * (n+1)
+    visited[0] = True
+    visited[1] = True
+    
+    distance = [0] * (n+1)
+    
+    graph = [[] for _ in range(n+1)]
+    for a, b in vertex:
         graph[a].append(b)
         graph[b].append(a)
-    queue = deque()
-    queue.append(1)
-    visited[1] = 1
+    # print(graph)
+    
+
+    queue = deque([1])
+
     while queue:
-        node = queue.popleft()
-        for n in graph[node]:
-            if visited[n] == 0:
-                visited[n] = visited[node] + 1
-                queue.append(n)
-    max_distance = max(visited)
-    for r in visited[1:]:
-        if r == max_distance:
-            answer += 1
-    return answer
+        v = queue.popleft()
+
+        for i in graph[v]:
+            if visited[i] == False:
+                visited[i] = True
+                queue.append(i)
+                distance[i] = distance[v] + 1
+    # print(distance)
+    
+    return distance.count(max(distance))
