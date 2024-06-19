df8c15cc62ff12f26b15780b950f3d075a1b4d20
Ethan_1000 <snowlaxc@gmail.com>
2024-06-19 16:35:20 +0900

    [level 2] Title: 할인 행사, Time: 0.03 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/2/131127. 할인 행사/할인 행사.py
+++ 프로그래머스/2/131127. 할인 행사/할인 행사.py
@@ -1,28 +1,17 @@
-from collections import deque, Counter
+from collections import Counter
 
 def solution(want, number, discount):
-    junghyeon = {}
-    need_day = sum(number)
-    full_day = len(discount)
-    discount_list = []
-    result = 0
+    answer = 0
+    need = {}    
+    for w, n in zip(want, number):
+        need[w] = n
     
-    for a, b in zip(want, number):
-        junghyeon[a] = b
-        
-    discount_count = Counter(discount)
+    total = sum(number)
+    day = len(discount)
     
-    for i in want:
-        if discount_count[i] < junghyeon[i]:
-            return result
-        
-    # print(junghyeon)
-    # print(discount_count)
-    
-    for j in range(0, (full_day) - need_day + 1):
-        discount_list.append(Counter(discount[j:j + need_day]))
-    
-    for k in discount_list:
-        if k == junghyeon:
-            result += 1
-    return result
+    for i in range(day - total + 1):
+        temp = Counter(discount[i:i+total])
+        if need == temp:
+            answer += 1
+
+    return answer
